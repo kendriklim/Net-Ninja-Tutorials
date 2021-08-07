4a237b5efc6bb95c99de090ddd66a800e1f06272
kendrik <kendrik_lim@artc.a-star.edu.sg>
2021-08-07 18:28:32 +0800

    Net Ninja - Object Oriented JavaScript Tutorial
    11 rounds of tutorial
    - classes
    - functions
    - extends

---
+++ 1_ObjectOrientedJavascript/objects.js
@@ -0,0 +1,78 @@
+function User(email, name) {
+  this.email = email;
+  this.name = name;
+  this.online = false;
+}
+
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, "has logged in");
+};
+
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, "has logged out");
+};
+
+function Admin(...args) {
+  User.apply(this, args);
+  this.role = "super admin";
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (u) {
+  users = users.filter((user) => {
+    return user.email != u.email;
+  });
+};
+
+var userOne = new User("ryu@ninjas.com", "Ryu");
+var userTwo = new User("yoshi@ninjas.com", "Yoshi");
+var admin = new Admin("abc@email.com", "abc");
+
+var users = [userOne, userTwo, admin];
+
+console.log(admin);
+
+// // ------USING CLASSES------
+
+// class User {
+//   constructor(email, name) {
+//     this.email = email;
+//     this.name = name;
+//     this.score = 0;
+//   }
+//   login() {
+//     console.log(this.email, "just logged in");
+//     return this;
+//   }
+//   logout() {
+//     console.log(this.email, "just logged out");
+//     return this;
+//   }
+//   updateScore() {
+//     this.score++;
+//     console.log(this.email, "score is now", this.score);
+//     return this;
+//   }
+// }
+
+// // cycle through user, true = keep user, false = remove user)
+// class Admin extends User {
+//   deleteUser(user) {
+//     users = users.filter((u) => {
+//       return u.email != user.email;
+//     });
+//   }
+// }
+
+// var userOne = new User("ryu@ninjas.com", "Ryu");
+// var userTwo = new User("yoshi@ninjas.com", "Yoshi");
+// var admin = new Admin("shaun@email.com", "shaun");
+
+// var users = [userOne, userTwo, admin];
+
+// admin.deleteUser(userOne);
+
+// console.log(users);
